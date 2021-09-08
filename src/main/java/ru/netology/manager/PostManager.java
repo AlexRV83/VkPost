38ec35e325da16368ced0de3827d68225e4c263a
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-09-08 10:55:20 +0300

    Second task of homework

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,21 @@
+package ru.netology.manager;
+
+import ru.netology.domain.VkPost;
+
+public class PostManager {
+    private VkPost[] vkPosts();
+
+    private WallSearch wallSearch;
+
+    public VkPost[] wallSearch() {
+
+        return null;
+    }
+    private WallDelete wallDelete;
+    public void wallDelete(){
+
+    }
+
+}
+
+
---
+++ src/main/java/ru/netology/manager/WallSearch.java
@@ -0,0 +1,58 @@
+package ru.netology.manager;
+
+public class WallSearch {
+    private int ownerId;
+    private String domain;
+    private String query;
+    private boolean ownersOnly;
+    private int count;
+    private int offset;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public boolean isOwnersOnly() {
+        return ownersOnly;
+    }
+
+    public void setOwnersOnly(boolean ownersOnly) {
+        this.ownersOnly = ownersOnly;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
