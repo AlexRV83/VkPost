e63c7a22a26e5f00160688453905321a7fba15c0
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-09-10 13:58:53 +0300

    revision 3

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -2,90 +2,19 @@ package ru.netology.manager;
 
 import ru.netology.domain.VkPost;
 
+package ru.netology.manager;
+
+
+
 public class PostManager {
-    private VkPost[] vkPosts();
-
-    public int ownerId;
-    public String domain;
-    public String query;
-    public boolean ownersOnly;
-    public int count;
-    public int offset;
-    public int postId
-
-    public int getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(int ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public boolean isOwnersOnly() {
-        return ownersOnly;
-    }
-
-    public void setOwnersOnly(boolean ownersOnly) {
-        this.ownersOnly = ownersOnly;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    public int getOffset() {
-        return offset;
-    }
-
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
-
-    public VkPost[] wallSearch() {
-
+    public VkPost[] wallSearch(int ownerId, String domain, String query, int count, int offset) {
         return null;
-
-    }
-    public void wallDelete(){
-
-        public int getPostId() {
-            return postId;
-        }
-
-        public void setPostId(int postId) {
-            this.postId = postId;
-        }
-
-        public int getOwnerId() {
-            return ownerId;
-        }
-
-        public void setOwnerId(int ownerId) {
-            this.ownerId = ownerId;
-        }
-
     }
 
+    public void wallDelete( int ownerId, int postId){
+        return;
+    }
+}
 }
 
 
