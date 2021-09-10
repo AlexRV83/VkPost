5b3cc65de3f4e129134eeef329862df4dcaa335f
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-09-10 09:03:14 +0300

    correction version 1

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,14 +5,84 @@ import ru.netology.domain.VkPost;
 public class PostManager {
     private VkPost[] vkPosts();
 
-    private WallSearch wallSearch;
+    public int ownerId;
+    public String domain;
+    public String query;
+    public boolean ownersOnly;
+    public int count;
+    public int offset;
+    public int postId
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public boolean isOwnersOnly() {
+        return ownersOnly;
+    }
+
+    public void setOwnersOnly(boolean ownersOnly) {
+        this.ownersOnly = ownersOnly;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 
     public VkPost[] wallSearch() {
 
         return null;
+
     }
-    private WallDelete wallDelete;
     public void wallDelete(){
+
+        public int getPostId() {
+            return postId;
+        }
+
+        public void setPostId(int postId) {
+            this.postId = postId;
+        }
+
+        public int getOwnerId() {
+            return ownerId;
+        }
+
+        public void setOwnerId(int ownerId) {
+            this.ownerId = ownerId;
+        }
 
     }
 
